2e4b41574c6bb57bf24d5886cd2229f6a369a587
Oleksandra Kuziura <radioday2014@gmail.com>
2024-12-20 15:55:17 +0200

    resolved problem

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const table = document.querySelectorAll('table')[0];
+
+const tableRows = table.rows;
+
+for (let i = 0; i < tableRows.length; i++) {
+  const cellCopy = tableRows[i].cells[1];
+  const cellAfter = tableRows[i].cells[4];
+  const newCell = document.createElement('td');
+
+  newCell.innerText = cellCopy.innerText;
+
+  cellAfter.before(newCell);
+}
